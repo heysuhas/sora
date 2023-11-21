7a00ba215563709cd9771eca9d22e796b081372c
Thammysetty Suhas <hey.suhas@outlook.in>
2023-11-21 10:28:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ import random
 app = Flask(__name__)
 
 responses = [
-    {"user_input": ["hello", "hi", "hey","namaste"], "bot_response": "Hey there!"},
-    {"user_input": ["what","your", "name"], "bot_response": "My name is Sora!"},
-    {"user_input": ["how", "are", "you"], "bot_response": "I'm great! Thanks for asking."},
-    {"user_input": ["bye", "goodbye"], "bot_response": "Goodbye!"},
-    {"user_input": ["I", "am", "fine","good"], "bot_response": "Glad to know!"},
-    {"user_input": ["who", "developed","you"], "bot_response": "I was developed by Suhas & Varun"},
-    {"user_input": ["who", "made","you"], "bot_response": "I was developed by Suhas & Varun"},
+    {"user_input": ["hello", "hi", "hey","namaste"], "bot_response": "Hey there! How's it going?😄"},
+    {"user_input": ["purpose"], "bot_response": "Hey! I'm here to chat, have fun and help you with anything you need. What's up?"},
+    {"user_input": ["color"], "bot_response": "Oh! I love talking about colors! I don't have a specific favourite colors, But I prefer Blue"},
+    {"user_input": ["work"], "bot_response": "I analyze your input using natural language processing to generate responses."},
+    {"user_input": ["food"], "bot_response": "I don't eat, but I've heard good things about data bytes."},
+     {"user_input": ["old", "age"], "bot_response": "I don't have an age in the traditional sense. I'm always up-to-date with the latest information!"},
+     {"user_input": ["name", "your"], "bot_response": "My name is Sora!"},
+    {"user_input": ["how", "you"], "bot_response": "I'm doing great! Thanks for asking. What about you?"},
+    {"user_input": ["bye", "goodbye"], "bot_response": "Aw, already leaving? Well, it was nice chatting with you. Feel free to come back anytime if you want to talk again. Take care and have a great day! 😊👏"},
+    {"user_input": ["what", "can", "you", "do"], "bot_response": "I can chat with you, answer questions, tell jokes, and more! Feel free to ask."},
+    {"user_input": ["I", "am", "fine","good"], "bot_response": "That's great to hear! You wanna talk about something?😄 "},
+    {"user_input": ["thanks", "thank", "you"], "bot_response": "You're welcome! If you have more questions, feel free to ask."},
+    {"user_input": ["tell", "me", "a", "joke"], "bot_response": "Sure, here's one: Why did the computer go to therapy? It had too many bytes of emotional danage!"},
 ]
 
 def get_bot_response(user_input):
